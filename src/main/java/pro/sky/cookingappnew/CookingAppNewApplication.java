1263e652754b0886174e3d75d2eaa503c5280a55
tatiana <tatiana.tokmantseva@gmail.com>
2023-06-10 17:01:42 +0500

    Chek

---
+++ src/main/java/pro/sky/cookingappnew/CookingAppNewApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cookingappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingAppNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingAppNewApplication.class, args);
+    }
+
+}
