a6d34d19754ee645056969cecb08b2bfd326c3cc
tatianatok <115864690+tatianatok@users.noreply.github.com>
2023-08-23 15:04:11 +0500

    Update RecipeController.java

--- src/main/java/pro/sky/cookingappnew/controllers/RecipeController.java
+++ src/main/java/pro/sky/cookingappnew/controllers/RecipeController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@Tag(name = "Рецепты", description = "CRUD операции и другие эндпоинты для работы с рецептами")
 @RequestMapping("/recipe")
-@Tag(name = "Рецепты")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -20,20 +20,27 @@ public class RecipeController {
     }
 
     @PostMapping
-    @Operation (summary = "Добавить новый рецепт", description = "Добавить новый рецепт")
+    @Operation(summary = "Добавление рецепта",
+            description = "для добавления рецепта требуется названиие, время приготовления, список ингредиентов и шаги приготовления")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "рецепт был успешно добавлен",
+            content = {@Content(mediaType = "application/json")})})
     public Recipe addNewRecipe(@RequestBody Recipe recipe) {
         recipeService.addNewRecipe(recipe);
         return recipe;
     }
 
-    @GetMapping
-    @Operation (summary = "Получить рецепт по ID", description = "Получить рецепт по ID")
+    @GetMapping("{id}")
+    @Operation(summary = "Поиск рецепта", description = "нужно искать рецепт по id")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "рецепт найден")})
     public Recipe getRecipe(@RequestParam Long recipeId) {
         return recipeService.getRecipe(recipeId);
     }
 
-    @GetMapping("/all")
-    @Operation (summary = "Получить все рецепты", description = "Получить все рецепты")
+    @GetMapping
+    @Operation(summary = "Получение  всех рецептов",
+            description = "получение списка List всех рецептов")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "рецепты были успешно получены",
+            content = {@Content(mediaType = "формат List")})})
     public ResponseEntity<Map<Long, Recipe>> getAllRecipe()  {
         Map<Long, Recipe> recipe = recipeService.getAllRecipe();
         if (recipe == null) {
@@ -42,8 +49,10 @@ public class RecipeController {
         return ResponseEntity.ok(recipe);
     }
 
-    @PutMapping("/{recipeId}")
-    @Operation (summary = "Изменить рецепт", description = "Изменить рецепт")
+    @PutMapping("/{id}")
+    @Operation(summary = "Редактирование рецепта",
+            description = "можно редактировать по id как один параметр, так и несколько в том числе название, время приготовления, список ингредиентов и шаги приготовления")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "рецепт был успешно отредактирован")})
     public ResponseEntity<Recipe> putRecipe(@PathVariable Long recipeId, @RequestBody Recipe recipe) {
         Recipe recipe1 = recipeService.putRecipe(recipeId, recipe);
         if (recipe1 == null) {
@@ -52,8 +61,10 @@ public class RecipeController {
         return ResponseEntity.ok(recipe);
     }
 
-    @DeleteMapping("/{recipeId}")
-    @Operation(summary = "Удалить рецепт", description = "Удалить рецепт")
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление рецепта",
+            description = "можно удалить рецепт только по id")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "рецепт успешно удален")}
     public ResponseEntity<Void> deleteRecipe(@PathVariable Long recipeId) {
         if (recipeService.deleteRecipe(recipeId)) {
             return ResponseEntity.ok().build();
