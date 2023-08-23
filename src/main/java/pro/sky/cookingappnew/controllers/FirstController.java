87c2ae73cc6b7f62bda8783267addbb18989433e
tatianatok <115864690+tatianatok@users.noreply.github.com>
2023-08-23 14:56:10 +0500

    Update FirstController.java

--- src/main/java/pro/sky/cookingappnew/controllers/FirstController.java
+++ src/main/java/pro/sky/cookingappnew/controllers/FirstController.java
@@ -4,14 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Информация о проекте рецептов", description = "содержит информацию об авторе проекта, описании проекта и даты создания проекта")
 public class FirstController {
 
     @GetMapping
-    public String appStart () {//Первый должен обрабатывать запросы, и странице должен отображаться текст «Приложение запущено»
+    @Operation(summary = "Запуск приложения", description = "информация о том, что приложение успешно запущено")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "приложение успешно запущено")})
+    public String appStart () {
         return "Приложение запущено";
     }
 
     @GetMapping("/info")
+    @Operation(summary = "Информация о проекте", description = "содержит автора проекта, название проекта и дату создания")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "информация успешно найдена")})
     public String info () {
         return "Татьяна Токманцева, CookingApp, 05.06.2023, Любимые рецепты на каждый день";
     }
